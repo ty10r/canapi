879d2c9feae8803d2a45ea6989204ae73f12aed2
Tylor <tylorlouis@ucla.edu>
2014-11-26 05:17:36 -0800

    Build out endpoints for every proxied endpoint and respond to them

--- server/api-proxy.js
+++ server/api-proxy.js
@@ -9,6 +9,11 @@ var ApiProxy = exports.ApiProxy = function( router ) {
 	this.apiSchema = {};
 	this.INVPATHERROR = "Invalid API path";
 
+	// Regexes for fixing path formats
+	this.uriParamRegex = /\/\{[^\}]+\}/;
+	this.obReg = /\/\{/g;
+	this.cbReg = /\}/g;
+
 	// Store URI validation functions to stop from creating redundant middleware
 	this.URIParamTypes = {};
 }
@@ -18,7 +23,7 @@ ApiProxy.prototype.init = function ( callback ) {
 
 	raml.loadFile( 'github.raml' ).then( function( data ) {
 		scope.apiSchema = data;
-		scope.generateValidation();
+		scope.generateAPI();
 		callback();
 	}, function( error ) {
 		process.exit(1);
@@ -26,15 +31,66 @@ ApiProxy.prototype.init = function ( callback ) {
 };
 
 // Post-order traverse of Resource tree
-ApiProxy.prototype.traverseResTree = function( res, fn ) {
+ApiProxy.prototype.traverseResTree = function( res, fn, absPath ) {
+	var cleanPath = this.formatUriSegment(absPath + (res.relativeUri || ''));
+
 	if ( res.resources !== undefined ) {
 		for ( var i = 0; i < res.resources.length; i++ ) {
-			this.traverseResTree( res.resources[i], fn );
+			this.traverseResTree( res.resources[i], fn, cleanPath );
 		}
 	}
-	fn( res );
+	fn( res, cleanPath );
 	return;
 };
+
+// Generate whole list of endpoints and validation
+ApiProxy.prototype.generateAPI = function() {
+
+	this.traverseResTree( this.apiSchema, this.buildEndpoint.bind(this), '/api-github-com' )
+
+};
+
+ApiProxy.prototype.buildEndpoint = function( res, absPath ) {
+	// Do validation of uri params
+	this.validateResource( res );
+
+	// Create actual endpoint
+	if ( 'methods' in res ) {
+		for ( var i = 0; i < res.methods.length; i++ ) {
+			this.makeRoute( absPath, res.methods[i].method, res );
+		}
+	}
+
+}
+
+ApiProxy.prototype.makeRoute = function( absPath, method, res ) {
+	if ( method === 'get' ) {
+		this.router.get( absPath, function( request, response ) {
+			// TODO: validate reqest params
+			// TODO: make call to actual endpoint
+			// TODO: return response
+			console.log( 'You got ' + absPath );
+			api.JsonResponse( 'Get request recieved @: ' + absPath, response, 200 );
+			return;
+		});
+	} else if ( method === 'post' ) {
+		this.router.post( absPath, function( request, response ) {
+			// TODO: validate post params
+			// TODO: make call to actual endpoint
+			// TODO: return response
+			console.log( 'You posted to ' + absPath );
+			api.JsonResponse( 'Post recieved @: ' + absPath, response, 200 );
+			return;
+		});
+	}
+}
+
+//******************************************************
+//* Middleward Generation
+//* Section is to generate middle-ware for URI parameter
+//* type checking
+//******************************************************
+
 
 ApiProxy.prototype.validateResource = function ( res ) {
 	if ( ! res.relativeUri ) return;
@@ -47,18 +103,6 @@ ApiProxy.prototype.validateResource = function ( res ) {
 	return;
 }
 
-
-//******************************************************
-//* Middleward Generation
-//* Section is to generate middle-ware for URI parameter
-//* type checking
-//******************************************************
-
-ApiProxy.prototype.generateValidation = function() {
-
-	this.traverseResTree( this.apiSchema, this.validateResource.bind(this) )
-
-};
 
 ApiProxy.prototype.validateURIParam = function ( param, type ) {
 	var scope = this;
@@ -84,4 +128,16 @@ ApiProxy.prototype.validateURIStrInt = function ( param, isInt ) {
 
 ApiProxy.prototype.respondInvPath = function( ) {
 	api.JsonResponse(this.INVPATHERROR, {}, 400 );
+}
+
+
+//******************************************************
+//* Utility functions
+//******************************************************
+
+ApiProxy.prototype.formatUriSegment = function ( uriSegment ) {
+	// Build the endpoint path string, replace '/{param}' with '/:param'
+
+	return uriSegment.replace(this.obReg, '/:').replace(this.cbReg, '');
+
 }
